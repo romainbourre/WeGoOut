944f46200fd5d52f4f7b4cb600444dcdfea1ed20
Romain <romain.bourre+github@me.com>
2022-12-16 23:06:20 +0100

    update Dockerfile with php 8.2

--- docker/config/entrypoint.sh
+++ docker/config/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 set -e
-echo "clear_env = no" >> /etc/php/8.1/fpm/pool.d/www.conf
+echo "clear_env = no" >> /etc/php/8.2/fpm/pool.d/www.conf
 /etc/init.d/php8.1-fpm start && nginx
 tail -f application.log
